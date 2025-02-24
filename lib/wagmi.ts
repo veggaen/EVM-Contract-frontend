07d6cefe31b3ab645fd031809b16e355dcdd7e0e
Vetle Gudman <89751706+veggaen@users.noreply.github.com>
2025-02-25 00:54:06 +0100

    Updated new sol contract
    new contract launched, testing state of code and app

--- lib/wagmi.ts
+++ lib/wagmi.ts
@@ -16,6 +16,6 @@ export const config = createConfig({
 
 // Define contract addresses per chain
 export const CONTRACT_ADDRESSES = {
-  [sepolia.id]: "0x057E087C848a967911fF29Fd3eB0CF49bCa38D09", // New Sepolia address - OLD:0xA3AEbCFF15ae61f10B62D097F6C412DA06391BF2
+  [sepolia.id]: "0x28Cd2E75607c01B9F17132711D71Ee5dF7b4387a", // New Sepolia address - OLD:0xA3AEbCFF15ae61f10B62D097F6C412DA06391BF2
   [mainnet.id]: "0x73b62ea73714c132E783BC0bA8318CCE7862c77a", // Original Mainnet address
 } as const;
